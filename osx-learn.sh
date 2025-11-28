b40ff0284d4ed366d712f9fcecced7f585e58708
drylikov <denis.rylikov@protonmail.com>
2025-11-28 13:08:56 +0300

    osx-learn
    Add words to the OS X Spell Check dictionary .

---
+++ osx-learn.sh
@@ -0,0 +1,83 @@
+
+#!/bin/sh
+
+#
+# Usage.
+#
+
+usage () {
+cat << EOF
+
+  Usage: learn [options] <word> [language]
+
+  Options:
+
+    -h, --help     output usage information
+    -p, --print    output dictionary (default LocalDictionary)
+    -v, --version  output version
+
+  Examples:
+
+    $ learn npm
+    $ learn stringification en_GB
+    $ learn --print
+    npm
+    $ learn --print en_GB
+    stringification
+
+  See also: man 1 learn
+EOF
+}
+
+#
+# Version.
+#
+
+version () {
+  echo "1.1.2"
+}
+
+#
+# Print.
+#
+
+print () {
+  readonly lang=${1-"LocalDictionary"}
+
+  database=~/Library/Spelling/$lang
+
+  cat "$database"
+}
+
+#
+# Options.
+#
+
+case $1 in
+  "")           usage; exit 1 ;;
+  -h|--help)    usage; exit ;;
+  -p|--print)   print $2; exit ;;
+  -v|--version) version; exit ;;
+  *)            readonly replace=$1 ; readonly lang=${2-"LocalDictionary"} ;;
+esac
+
+#
+# Find list.
+#
+
+database=~/Library/Spelling/$lang
+
+touch "$database"
+
+echo "$1" >> $database
+
+data=$(cat $database | sort | uniq)
+
+echo "$data" > $database
+
+#
+# Restart AppleSpell.
+#
+
+launchctl stop com.apple.applespell
+launchctl start com.apple.applespell
---
+++ readme.md
@@ -0,0 +1,48 @@
+
+# osx_learn
+
+![`bin/sh`][bash]
+
+> Add words to OS X’s valid words list, as in, remove warnings about words
+> like `npm`.
+
+Tested on OS X 10.10 (Yosemite), 10.13 (High Sierra), 10.14 (Mojave), might
+work earlier.
+
+If a language is specified, the language designator (ISO 639-1 or ISO 639-2)
+must be lower-case, e.g., `nl`.
+When a region designator (ISO 3166-1) is added, it must be all-caps and
+preceded by an underscore, e.g., `en_GB`.
+
+Ensure the language is enabled in `System Preferences` > `Keyboard` >
+`Text` > `Spelling` > `Set Up...`.
+
+When adding a word to a language which has region designators (such as English,
+which has British and more), **YOU MUST SPECIFY THE REGION TOO**.
+In the case of British, specifying `en` will not work, but `en_GB` will.
+
+## Install
+
+[npm][]:
+
+```bash
+npm install osx-learn --global
+```
+
+## Usage
+
+```text
+Usage: learn [options] <word> [language]
+
+Options:
+
+  -h, --help     output usage information
+  -v, --version  output version
+
+Examples:
+
+  $ learn npm
+  $ learn stringification en_GB
+
+See also: man 1 learn
+```
